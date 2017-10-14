9506dd7de548021c108e8a4535a2cbd0bbb84809
José Antonio García <jagarciavi1992@gmail.com>
2017-10-14 20:38:07 +0200

    Added option for updating brew casks
    Using buo/cask-upgrade for updating apps

--- keepUp.sh
+++ keepUp.sh
@@ -1,4 +1,21 @@
 #!/bin/bash
 #Auto updater/upgrader/cleaner for macOS systems (with homebrew installed)
 
-brew doctor && brew update && brew upgrade && brew cleanup
+# First we search if tool for updating casks is installed 
+
+if brew tap | grep buo/cask-upgrade; then
+    # Do nothing, is installed
+else 
+    echo "Installing brew-cask-upgrade"
+    brew tap buo/cask-upgrade
+fi
+# Tool for cask updates is installed, now the party starts
+echo "Upgrading..."
+# Check for updates
+brew update
+# Upgrade all packages from brew and taps
+brew upgrade
+# Upgrade all cask - also autoupdated apps and 'latest' apps -> https://github.com/buo/homebrew-cask-upgrade
+brew cu -a -f -y
+# Clean up all the mess
+brew cleanup
